042f57b57facdc6da99e25cdd8c62fd9e2a5a14a
Faith Shatto <shat5060@m506-118-26446.local>
2019-02-25 14:16:14 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Faith Shatto on 2/25/19.
+//  Copyright © 2019 Faith Shatto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
