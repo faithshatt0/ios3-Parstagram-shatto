56e584288fbd759c59d4ffdfa8d18b5a1dec439f
Faith Shatto <shat5060@m506-118-26446.local>
2019-03-04 15:52:39 -0800

    can now see comments

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,27 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Faith Shatto on 3/4/19.
+//  Copyright © 2019 Faith Shatto. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
